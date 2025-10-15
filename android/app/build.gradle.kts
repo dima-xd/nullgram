977d2874871a783552f39e9f6bb868e0fb1a127e
Dzmitry Dubrova <dimaxdqwerty@gmail.com>
2025-10-15 16:06:53 +0300

    Misc improvements + CI/CD fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,10 +30,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
-        }
     }
 
     signingConfigs {
